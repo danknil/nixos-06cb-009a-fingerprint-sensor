42ad5b38396c1ed5b36f2012a0ad88d0f055e858
danknil <danknil@protonmail.com>
2024-09-04 12:01:52 +0700

    fix convinient error

--- modules/default.nix
+++ modules/default.nix
@@ -1,10 +1,13 @@
-{ inputs, outputs }: rec {
+{
+  inputs,
+  outputs,
+}: rec {
   python-validity = import ./python-validity outputs;
   open-fprintd = import ./open-fprintd outputs;
   default = _: {
     imports = [
-      (python-validity outputs)
-      (open-fprintd outputs)
+      python-validity
+      open-fprintd
     ];
   };
 }
